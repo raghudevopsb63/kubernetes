c531992ac4a8fd949b285671eae733f13409aef2
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-22 15:31:49 +0530

    Update kubernetes/

--- eks/main.tf
+++ eks/main.tf
@@ -1,5 +1,6 @@
 module "eks" {
-  source = "./vendor/modules/eks"
-  ENV    = var.ENV
+  source             = "./vendor/modules/eks"
+  ENV                = var.ENV
+  PRIVATE_SUBNET_IDS = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 }
 
