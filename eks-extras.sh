c46573ab9b1aaf747785765675047a5cca59c36a
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-27 08:07:03 +0530

    Update kubernetes/

--- eks-extras.sh
+++ eks-extras.sh
@@ -5,3 +5,4 @@ helm repo add elastic https://helm.elastic.co
 helm repo update
 helm install ng-ingress nginx-stable/nginx-ingress
 helm install filebeat elastic/filebeat -f filebeat.yml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
