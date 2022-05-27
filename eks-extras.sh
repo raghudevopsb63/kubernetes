4bb91d2b0054bc5a5a5fd226fd2796b607793e22
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-27 07:13:56 +0530

    Update kubernetes/

--- eks-extras.sh
+++ eks-extras.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 helm repo add nginx-stable https://helm.nginx.com/stable
+helm repo add elastic https://helm.elastic.co
 helm repo update
 helm install ng-ingress nginx-stable/nginx-ingress
+helm install filebeat elastic/filebeat -f filebeat.yml
